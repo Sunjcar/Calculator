e50847dddd9071077d2af685dde1a59ffcccde23
Raymond Chen <raychen358@gmail.com>
2022-04-11 12:11:00 -0700

    Added eventlisteners that take input, display results, and reset Calculator.

--- calc.js
+++ calc.js
@@ -8,6 +8,13 @@ const btnSeven = document.getElementById('7');
 const btnEight = document.getElementById('8');
 const btnNine = document.getElementById('9');
 const btnZero = document.getElementById('0');
+const btnAdd = document.getElementById('+');
+const btnMult = document.getElementById('*');
+const btnSub = document.getElementById('-');
+const btnDivide = document.getElementById('/');
+const btnEqual = document.getElementById('=')
+const btnClear = document.getElementById('clear')
+const operator = document.querySelectorAll('.operator')
 
 btnOne.addEventListener('click',clickListener);
 btnTwo.addEventListener('click',clickListener);
@@ -19,13 +26,19 @@ btnSeven.addEventListener('click',clickListener);
 btnEight.addEventListener('click',clickListener);
 btnNine.addEventListener('click',clickListener);
 btnZero.addEventListener('click',clickListener);
+btnAdd.addEventListener('click',Operator);
+btnMult.addEventListener('click',Operator);
+btnSub.addEventListener('click',Operator);
+btnDivide.addEventListener('click',Operator);
+btnEqual.addEventListener('click', results);
+btnClear.addEventListener('click',reset)
 
-let num1 = '';
-let clickedOperator = ''
+let storedNum = '';
+let clickedOperator = ' '
 let firstNumber = '';
 let result = '';
-inputOne.textContent = 0;
-
+inputOne.textContent = ' ';
+inputTwo.textContent = ' ';
 
 
 //add function
@@ -33,41 +46,62 @@ function add(a,b){
    let x = a + b;
        return x;
 
-};console.log(add(3,5));
+};
 
 //subtract function
 function sub(a,b){
     let x = a - b;
     return x;
-}console.log(sub(9,2))
+};
 
 //multiply function
  function multiply(a,b){
     let x = a * b;
     return x;
-}  console.log(multiply(8,4))
+};
 
 //divide function
 function divide(a,b){
     let x = a / b;
     return x;
-};console.log(divide(10,3))
+};
 
 //Calls one of the functions above
-function operate(a,b,operator){
-    if (operator === "+"){
-        add(a,b);
-    } else if (operator === "-"){
-        sub(a,b);
-    } else if (operator === "*"){
-        multiply(a,b)
-    } else if (operator === "a/b"){
-        divide(a,b)
+function operate(a,b,clickedOperator){
+    if (clickedOperator === "+"){
+       return add(a,b);
+    } else if (clickedOperator === "-"){
+       return sub(a,b);
+    } else if (clickedOperator === "*"){
+       return multiply(a,b)
+    } else if (clickedOperator === "/"){
+        return divide(a,b)
     }
 }
-
+//Takes initial user input via # button
 function clickListener(event){
-    let x = event.target.id;
-    inputOne.textContent = x;
-    num1 = inputOne.textContent;
+    storedNum = event.target.id; 
+    inputOne.textContent = storedNum;
+}
+
+//Stores initial input and takes operator input
+function Operator(event){
+  firstNumber = storedNum;
+  clickedOperator = event.target.id;
+  operator.textContent = clickedOperator; 
+  inputOne.textContent = operator.textContent
+  storedNum = '';
+}
+
+ //Calls function corresponding to the operator clicked and returns value.
+ function results(){
+   result = operate(parseFloat(firstNumber),parseFloat(storedNum),clickedOperator);
+    inputOne.textContent = result; 
+    storedNum = result;
+}
+
+//Resets textContent
+function reset(){
+    inputOne.textContent = ' ';
+    inputTwo.textContent = ' '; 
 }
